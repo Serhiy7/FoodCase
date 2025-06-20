7d09c1c858e78baff70733e94ee5d881da867296
Serhiy7 <ssukalo79@gmail.com>
2025-06-20 21:27:26 +0200

    2

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; 
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 const root = document.getElementById("root");
 
 createRoot(root).render(
-  <React.StrictMode basename='ReactFoodCase'>
+  <React.StrictMode basename="FoodCase">
     <BrowserRouter>
       <App />
     </BrowserRouter>
